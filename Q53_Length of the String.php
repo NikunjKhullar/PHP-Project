31aa7dd8bcbc4d16d0d61c001f05cb8a8a05f7eb
NikunjKhullar131 <nikunjkhullar@gmail.com>
2025-04-09 23:31:45 +0530

    Add files via upload

---
+++ Q53_Length of the String.php
@@ -0,0 +1,8 @@
+<?php
+
+    $my_str = "Welcome to Nikunj's GitHub";
+     
+    // Calculating and displaying string length
+    echo strlen($my_str);
+    echo"<br>This code is executed by Nikunj Khullar (0221BCA131)";
+?>
